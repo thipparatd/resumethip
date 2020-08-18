544e12856f140dfd930c65e0ba9a7d71b05bf41b
ThipparatD <oatcraft09@gmail.com>
2020-08-18 23:10:03 +0700

    asd

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+})
+
+document.querySelector("");
